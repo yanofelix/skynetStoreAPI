b139252baa5d9c5527d0a25a76b9d227e134441b
yanofelix <devyanfelix@gmail.com>
2022-07-17 18:25:44 -0300

    first commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {      
+            public DataContext (DbContextOptions<DataContext> options) : base (options)
+            {
+
+            }
+
+        public DbSet<Product> Products { get; set; }
+
+        }
+    
+}
