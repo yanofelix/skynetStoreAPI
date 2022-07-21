a6cca07c2cb61baa1ff6c06cae483cc6f17e56ba
yanofelix <devyanfelix@gmail.com>
2022-07-21 10:33:56 -0300

    repository patern, static files, specifications done

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductDTO, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            this._config = config;
+        }
+        public string Resolve(Product source, ProductDTO destination, string destMember,
+            ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
